204459e5cc1635ad848702dd705706f76323a1dd
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-10-30 20:02:47 -0300

    feat: add signup factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,15 @@
+import SignUpController from '../../presentation/controllers/signup/signup'
+import { DbAddAccount } from './../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from './../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from './../../infra/db/mongodb/account-repository/account'
+import { EmailValidatorAdapter } from './../../utils/email-validator-adapter'
+
+export const makeSignupController = (): SignUpController => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return signUpController
+}
