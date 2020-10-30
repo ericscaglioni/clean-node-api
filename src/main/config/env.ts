17e13a2ce514dff8d147056c09304387843f71c4
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-10-30 19:50:02 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  port: process.env.PORT || 5050,
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://dbUser:dbUserSenhaSecreta@cluster0.1bxdk.mongodb.net/clean-node-api?retryWrites=true&w=majority'
+}
