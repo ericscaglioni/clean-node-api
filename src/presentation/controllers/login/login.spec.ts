55ae418103d3ec34313afd60a25d26029b757be3
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-10-31 14:41:48 -0300

    test: ensure logincontroller returns status code 400 when no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,28 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { HttpRequest } from './../../protocols/http'
+import { LoginController } from './login'
+
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
+}
+
+describe('Login Controller', () => {
+  test('Should return status code 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
