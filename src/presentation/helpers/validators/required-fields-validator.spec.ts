4364bd1499c056f064fcecd6e7a9543729b60a01
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-11-07 15:26:45 -0300

    test: ensure requiredfieldsvalidation not return if validation succeeds

--- src/presentation/helpers/validators/required-fields-validator.spec.ts
+++ src/presentation/helpers/validators/required-fields-validator.spec.ts
@@ -7,4 +7,10 @@ describe('Required Fields Validator', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldsValidator(['any_field'])
+    const error = sut.validate({ any_field: 'field' })
+    expect(error).toBeFalsy()
+  })
 })
