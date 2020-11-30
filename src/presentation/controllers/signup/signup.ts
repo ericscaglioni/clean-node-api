73ea1c71e84f4a0ec5e402474d65117da920a574
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-11-30 18:54:30 -0300

    feat: ensure SignupController returns 409 if user already exists

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,10 +1,12 @@
-import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
-import { AddAccount, Controller, HttpRequest, HttpResponse, Validator } from '../signup/signup-protocols'
+import { badRequest, conflict, ok, serverError } from '../../helpers/http/http-helper'
+import { AddAccount, Controller, ExistingEmailValidator, HttpRequest, HttpResponse, Validator } from '../signup/signup-protocols'
+import { UserAlreadyExistsError } from './../../errors'
 
 export default class SignUpController implements Controller {
   constructor (
     private readonly addAccount: AddAccount,
-    private readonly validator: Validator
+    private readonly validator: Validator,
+    private readonly existingEmailValidator: ExistingEmailValidator
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -15,6 +17,11 @@ export default class SignUpController implements Controller {
       }
 
       const { name, email, password } = httpRequest.body
+
+      const emailExists = await this.existingEmailValidator.emailAlreadyExists(email)
+      if (emailExists) {
+        return conflict(new UserAlreadyExistsError())
+      }
 
       const account = await this.addAccount.add({
         name,
