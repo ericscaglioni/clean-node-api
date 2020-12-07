14c7a18ced1931cb0c51db0ac400c3d20f13094a
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-07 19:01:18 -0300

    feat: ensure AddSurveyController calls Validator with correct value

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -0,0 +1,45 @@
+import { AddSurveyController } from './add-survey-controller'
+import { HttpRequest, Validator } from './add-survey-controller-protocols'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }]
+  }
+})
+
+const makeValidator = (): Validator => {
+  class ValidatorStub implements Validator {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidatorStub()
+}
+
+interface SutTypes {
+  sut: AddSurveyController
+  validatorStub: Validator
+}
+
+const makeSut = (): SutTypes => {
+  const validatorStub = makeValidator()
+  const sut = new AddSurveyController(validatorStub)
+  return {
+    sut,
+    validatorStub
+  }
+}
+
+describe('Add Survey Controller', () => {
+  test('Should call Validator with correct data', async () => {
+    const { sut, validatorStub } = makeSut()
+    const validateSpy = jest.spyOn(validatorStub, 'validate')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+})
