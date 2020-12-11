d5f4c1712819e67979a47eee9106ecb48fff3acf
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-10 22:26:07 -0300

    refactor: organize main layer folders

--- src/main/factories/controllers/login/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login/login-controller-factory.ts
@@ -1,7 +1,7 @@
 import { LoginController } from '../../../../../presentation/controllers/login/login/login-controller'
 import { Controller } from '../../../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
-import { makeDbAuthentication } from '../../../usecases/authenticator/db-authentication-factory'
+import { makeDbAuthentication } from '../../../usecases/account/authenticator/db-authentication-factory'
 import { makeLoginValidator } from './login-validator-factory'
 
 export const makeLoginController = (): Controller => {
--- src/main/factories/controllers/login/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -1,8 +1,8 @@
 import SignUpController from '../../../../../presentation/controllers/login/signup/signup-controller'
 import { Controller } from '../../../../../presentation/protocols/controller'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
-import { makeDbAddAccount } from '../../../usecases/add-account/db-add-account-factory'
-import { makeDbAuthentication } from '../../../usecases/authenticator/db-authentication-factory'
+import { makeDbAddAccount } from '../../../usecases/account/add-account/db-add-account-factory'
+import { makeDbAuthentication } from '../../../usecases/account/authenticator/db-authentication-factory'
 import { makeSignUpValidator } from './signup-validator-factory'
 
 export const makeSignupController = (): Controller => {
