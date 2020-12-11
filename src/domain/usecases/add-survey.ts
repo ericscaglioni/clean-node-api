8f3db53064903ff765b399e3ef215e55f8e84cd7
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-11 09:40:54 -0300

    feat: ensure LoadSurveysController calls LoadSurveys Usecase

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,12 +1,9 @@
+import { SurveyAnswer } from '../models/survey'
+
 export interface AddSurveyModel {
   question: string
   answers: SurveyAnswer[]
   date: Date
-}
-
-interface SurveyAnswer {
-  image?: string
-  answer: string
 }
 
 export interface AddSurvey {
