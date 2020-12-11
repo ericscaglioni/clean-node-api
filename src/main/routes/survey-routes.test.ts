306d1395f875a8a0e0aa68d77290c7945dac6f73
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-10 23:06:33 -0300

    feat: ensure add survey returns 204 if access token and user role are provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import env from '../config/env'
+import { sign } from 'jsonwebtoken'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /survey', () => {
@@ -33,6 +38,37 @@ describe('Survey Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with access token for an admin role', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Eric',
+        email: 'ericsf34@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/survey')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+          }, {
+            answer: 'other_answer'
+          }]
+        })
+        .expect(204)
     })
   })
 })
